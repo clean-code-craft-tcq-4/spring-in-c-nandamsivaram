fe99e9d6658aed98c724b619a1c3443ce788271b
nandamsivaram <84092480+nandamsivaram@users.noreply.github.com>
2022-05-18 18:58:24 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,23 @@
 #include "stats.h"
+#include "math.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-}
+	s.min = numberset[0];
+	s.max = numberset[0];
+	float var = 0;
+		for(int i = 0; i < setlength ; i++)
+		{
+			var += numberset[i];
+			/**Get the min value**/
+			if(s.min > numberset[i] )
+				s.min = numberset[i];
+			/**Get the max value**/
+			if(s.max < numberset[i] )
+				s.max = numberset[i];
+		}/*end of for loop*/
+		s.average = var / setlength;
+		
+	return s;
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+}

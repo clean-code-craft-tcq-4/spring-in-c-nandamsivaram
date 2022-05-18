9ebe6a7f55b4f8dcebfcaa29487580ef9b2b6f07
nandamsivaram <84092480+nandamsivaram@users.noreply.github.com>
2022-05-18 19:48:05 +0530

    Update Alert.h

--- Alert.h
+++ Alert.h
@@ -1,10 +1,10 @@
 #define NO_OF_ALERTS 2
+
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
 
-typedef void (*alerter_funcptr[NO_OF_ALERTS])(void);
+typedef void (*alerter_funcptr)(void);
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 void emailAlerter(void);
 void ledAlerter(void);
-
